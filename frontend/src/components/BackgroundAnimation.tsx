1a0776ca94d4bd154942ef2d32e001193802b0e3
Rafael Macedo <rafaelrodriguesmacedo2004@gmail.com>
2023-07-19 13:56:33 -0300

    correct color animation

--- frontend/src/components/BackgroundAnimation.tsx
+++ frontend/src/components/BackgroundAnimation.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { useRef, useEffect } from 'react';
+import '../styles/animation.css'
 
 const BackgroundAnimation: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -8,13 +9,13 @@ const BackgroundAnimation: React.FC = () => {
   useEffect(() => {
     if (!canvasRef.current) return;
 
-    const ctx = canvasRef.current.getContext('2d');
-    let t = 0;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
 
     const col = (x: number, y: number, r: number, g: number, b: number) => {
-      if (!ctx) return;
-      ctx.fillStyle = `rgb(${r},${g},${b})`;
-      ctx.fillRect(x, y, 1, 1);
+      if (!context) return;
+      context.fillStyle = `rgb(${r},${g},${b})`;
+      context.fillRect(x, y, 1, 1);
     };
 
     const R = (x: number, y: number, t: number) => {
@@ -22,27 +23,22 @@ const BackgroundAnimation: React.FC = () => {
     };
 
     const G = (x: number, y: number, t: number) => {
-      return Math.floor(
-        192 + 64 * Math.sin((x * x * Math.cos(t / 4) + y * y * Math.sin(t / 3)) / 300)
-      );
+      return 0; // Defina o componente verde (g) como 0
     };
 
     const B = (x: number, y: number, t: number) => {
-      return Math.floor(
-        192 + 64 * Math.sin(5 * Math.sin(t / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100)
-      );
+      return Math.floor(192 + 64 * Math.sin(5 * Math.sin(t / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100));
     };
 
-    const run = () => {
-      if (!ctx) return;
+    let t = 0;
 
+    const run = () => {
       for (let x = 0; x <= 35; x++) {
         for (let y = 0; y <= 35; y++) {
-          col(x, y, R(x, y, t), G(x, y, t), B(x, y, t));
+          col(x, y, R(x, y, t), 0, B(x, y, t));
         }
       }
-
-      t = t + 0.02;
+      t += 0.02;
       window.requestAnimationFrame(run);
     };
 
@@ -50,15 +46,10 @@ const BackgroundAnimation: React.FC = () => {
   }, []);
 
   return (
-    <div className='w-full h-full bg-gray-600'>
-        <canvas
-            ref={canvasRef}
-            id="canv"
-            className='w-full h-1/4'
-        />
+    <div id='canvas-container'>
+        <canvas ref={canvasRef}/>
     </div>
   );
-  
 };
 
 export default BackgroundAnimation;
