ebd51a431ffa2a6de3578d946699895ea7f573e0
Rafael Macedo <rafaelrodriguesmacedo2004@gmail.com>
2023-08-17 13:53:45 -0300

    form update

--- frontend/src/app/formulario/page.tsx
+++ frontend/src/app/formulario/page.tsx
@@ -1,43 +1,62 @@
 import Header from "@/components/Header";
 
 export default function FormPage() {
-    return (
-        <>
-            <Header />
-            <section className="m-auto text-center">
-                <h1 className="text-6xl mt-[20vh] font-bold">
-                    Formulário de Contato
-                </h1>
-                <form action="" method="get" className="">
-                    <label
-                        htmlFor=""
-                        className="flex flex-col text-lg">
-                        E-mail
-                    </label>
-                    <input
-                        type="text" name="email-field" id=""
-                        className="bg-gray-300 flex flex-col w-full px-2 py-2 text-xl" required />
-                    <input type="checkbox" name="isChecked" id="isChecked" value="true" className="rounded border-gray-400 bg-gray-700 text-purple-500" />
-                        Enviar Mensagem Anônima
-                    <label
-                        htmlFor=""
-                        className="flex flex-col pt-4 text-lg">
-                        Assunto
-                    </label>
-                    <input type="text" name="subject-field" id="subject-field"
-                        className="bg-gray-300 flex flex-col w-full px-2 py-2 text-xl" required />
-                    <label
-                        htmlFor=""
-                        className="flex flex-col pt-4 text-lg">
-                        Mensagem
-                    </label>
-                    <input type="text" name="message-field" id="message-field"
-                        className="bg-gray-300 flex flex-col w-full px-2 py-2 text-xl" required />
-                </form>
-                <div className="mt-4">
-                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Enviar</button>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <section className="bg-gray-100 min-h-screen flex items-center justify-center">
+        <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+          <h1 className="text-4xl font-bold text-center mb-6">
+            Formulário de Contato
+          </h1>
+          <form
+            action="https://formsubmit.co/rafaelrodriguesmacedo2004@gmail.com"
+            method="POST"
+            className="space-y-4"
+          >
+            <div className="flex flex-col">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                placeholder="E-mail"
+                className="input"
+              />
+            </div>
+            <div className="flex flex-col">
+              <input
+                type="text"
+                name="subject"
+                id="subject"
+                required
+                placeholder="Assunto"
+                className="input"
+              />
+            </div>
+            <div className="flex flex-col">
+              <textarea
+                name="message"
+                id="message"
+                required
+                placeholder="Mensagem"
+                className="input"
+              ></textarea>
+            </div>
+            <input
+              type="hidden"
+              name="_next"
+              value="http://localhost:3000/obrigado"
+            />
+            <button
+              type="submit"
+              className="text-white px-4 py-2 rounded button bg-blue-500 hover:bg-blue-600"
+            >
+              Enviar
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
---
+++ frontend/src/app/obrigado/page.tsx
@@ -0,0 +1,17 @@
+import Header from "../../components/Header";
+
+export default function ObrigadoPage() {
+    return (
+        <>
+            <Header />
+            <section className="m-auto text-center">
+                <h1 className="text-6xl mt-[20vh] font-bold">
+                    Obrigado por entrar em contato!
+                </h1>
+                <h2 className="text-5xl mt-[20vh]">
+                    Responderemos o mais rápido possível.
+                </h2>
+            </section>
+        </>
+    )
+}
