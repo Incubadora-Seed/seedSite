7f098e1c1e0386fad1a1060b6cd093990f7585b2
Rafael Macedo <rafaelrodriguesmacedo2004@gmail.com>
2023-09-15 16:01:17 -0300

    pdf visualization finished

--- server/src/routes/download.ts
+++ server/src/routes/download.ts
@@ -25,24 +25,26 @@ export async function downloadRoutes(server: FastifyInstance) {
     });
 
     server.get('/download/:filename', async (request, reply) => {
-            try {
-                const params = request.params as RouteParams;
-                const filename = params.filename;
-                const filePath = path.join(__dirname, '..', 'uploads', filename);
-    
-                const fileExists = fs.existsSync(filePath);
-                if (!fileExists) {
-                    return reply.status(404).send({ error: 'File not found' });
-                }
-    
-                reply.header('Content-Disposition', `attachment; filename="${filename}"`);
-                reply.type('application/pdf');
-    
-                const fileStream = fs.createReadStream(filePath);
-                reply.send(fileStream); // Usando o método send para enviar o arquivo
-            } catch (error) {
-                console.error('Error during download:', error);
-                return reply.status(500).send({ error: 'Internal server error' });
+        try {
+            const params = request.params as RouteParams;
+            const filename = params.filename;
+            const filePath = path.join(__dirname, '..', 'uploads', filename);
+
+            const fileExists = fs.existsSync(filePath);
+            if (!fileExists) {
+                return reply.status(404).send({ error: 'File not found' });
             }
-        });
+
+            reply.header('Content-Disposition', `inline; filename="${filename}"`);
+            reply.type('application/pdf');
+
+            const fileContent = await fs.promises.readFile(filePath);
+            reply.send(fileContent);
+        } catch (error) {
+            console.error('Error during download:', error);
+            return reply.status(500).send({ error: 'Internal server error' });
+        }
+    });
+    
+    
 }
